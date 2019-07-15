d9f01f165fc7f8f5a756f15664c67b2aa9c9f90c
fengrixin <fengrixin14@gmail.com>
2019-07-15 15:52:31 +0800

    change babel and plugins

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import { Button } from "ant-design-vue";
+import "ant-design-vue/dist/antd.less";
+
+Vue.use(Button);
 
 Vue.config.productionTip = false;
 
